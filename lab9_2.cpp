28cc651e3ee2b2433c5eca34661cdc0ceb1c0a30
nayrunner <59276659+nayrunner@users.noreply.github.com>
2020-01-28 15:58:34 +0700

    Update lab9_2.cpp

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,4 +1,22 @@
-//cout << "-------------------- SOTUS ---------------------";
-//cout << textline;
-//cout << "-------------------- SOTUS ---------------------";
-//Don't forget to change cout to output filestream
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+
+int main(){
+    string textline;
+    bool havetext;
+    
+    ofstream dest;
+    dest.open("C:\\Users\\Admin\\Desktop\\c##\\cheerbook_copy.txt");
+   
+    ifstream source;
+    source.open ("C:\\Users\\Admin\\Desktop\\c##\\git\\lab9-2562-2-nayrunner\\cheerbook.txt");
+    havetext = getline(source, textline);
+    dest << "-------------------- SOTUS ---------------------\n";
+    while(havetext){
+        dest<<textline<<"\n";
+        havetext = getline(source, textline);
+    }
+    dest << "-------------------- SOTUS ---------------------";
+}
