524d432929ae944709132c9176fa2ac984fa9d72
nayrunner <59276659+nayrunner@users.noreply.github.com>
2020-01-28 16:23:06 +0700

    Update lab9_3.cpp

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,35 @@
+#include<fstream>
+#include<iostream>
+#include<math.h>
+#include<string>
+#include<cstdlib>
+using namespace std;
+ 
+int main(){
+  int count= 0 ;
+  float sum= 0 ,sumsum = 0;
+  double mean ,sd ;
+  string textline;
+  ifstream source ("C:\\Users\\Admin\\Desktop\\c##\\git\\lab9-2562-2-nayrunner\\score.txt");
+  while (getline(source,textline)){
+      cout<<textline<<'\n';
+      sum +=atof(textline.c_str());
+      sumsum += (atof(textline.c_str())*atof(textline.c_str()));
+
+      count++;
+  }
+
+  
+  mean = sum/count;
+  cout<<"Number of data = "<<count<<endl;
+  cout<<"Mean = "<<mean<<endl;
+  sd =(sumsum/count)-(mean*mean);
+  cout<<"Standard deviation = "<<sqrt(sd);
+  source.close();
+  
+  
+  return 0;
+}
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
